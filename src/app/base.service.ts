823b8b466ef9214740e953eebd2afbb6ada1178c
Erős István <eros@zold.lan>
2023-11-25 11:40:26 +0100

    11:40

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { finalize } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
+  basePath="/feltolt"
 
-  constructor() { }
+  constructor(private storage:AngularFireStorage) { }
+
+  pushFile(file:any){
+    const fullPath=this.basePath+"/"+file.name
+    const storegeRef= this.storage.ref(fullPath)
+
+    const uploadTask=this.storage.upload(fullPath,file)
+    
+    uploadTask.snapshotChanges().pipe(
+      finalize(
+        ()=>{
+          storegeRef.getDownloadURL().subscribe(
+            (url)=>console.log(url)
+          )
+      }
+      )
+    )
+    .subscribe(
+      (res)=>console.log(res)
+    )
+      return uploadTask.percentageChanges()
+  }
 }
--- src/app/upload-form/upload-form.component.ts
+++ src/app/upload-form/upload-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BaseService } from '../base.service';
 
 @Component({
   selector: 'app-upload-form',
@@ -7,13 +8,25 @@ import { Component } from '@angular/core';
 })
 export class UploadFormComponent {
   selectedFile:any
+  upload=false
+  percentage=0
 
+  constructor(private base:BaseService){}
 
   selectFile(event:any){
     console.log(event.target.files[0])
     this.selectedFile=event.target.files[0]
+    this.upload=false
+    this.percentage=0
   }
   uploadFile(){
     console.log("Én fogom feltölteni a filet!", this.selectedFile)
+    this.upload=true
+    this.base.pushFile(this.selectedFile).subscribe(
+      (res)=>{
+        console.log(res,"%")
+        this.percentage=Math.round(Number(res))
+      }
+    )
   }
 }
